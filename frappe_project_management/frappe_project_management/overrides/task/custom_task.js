3745b4903a3d7eb87cbf0ef6c02e2fb2988cc623
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2026-06-03 11:14:28 +0400

    feat: default complete details in task

---
+++ frappe_project_management/frappe_project_management/overrides/task/custom_task.js
@@ -0,0 +1,11 @@
+frappe.ui.form.on('Task', {
+	refresh(frm) {
+		// your code here
+	},
+    status: function (frm) {
+        if (frm.doc.status == "Completed"){
+            frm.set_value("completed_on",frappe.datetime.nowdate())
+            frm.set_value("completed_by",frappe.session.user)
+        }
+    }
+})
--- frappe_project_management/frappe_project_management/overrides/task/custom_task.py
+++ frappe_project_management/frappe_project_management/overrides/task/custom_task.py
@@ -3,5 +3,4 @@ from erpnext.projects.doctype.task.task import Task
 
 class CustomTask(Task):
     def validate(self):
-        pass
         frappe.msgprint("Validate")
